f69bd5714412d5f9f3f1151c6c685dabfaf13d1d
Stean Kotze <stephanus-johannes.kotze@vcontractor.co.za>
2021-03-02 12:57:08 +0200

    [Backend] Created folder structure for feature.

---
+++ app/controllers/trouble_ticket_controller.js
@@ -0,0 +1,34 @@
+//  === Requirements ===
+const faker = require('faker')
+
+//  === Controller ===
+module.exports = {
+    create: function(request)
+    {
+
+    },
+    index: function(request)
+    {
+
+    },
+    show: function(request)
+    {
+
+    },
+    update: function(request)
+    {
+
+    },
+    destroy: function(request)
+    {
+
+    },
+    create_hub: function(request)
+    {
+
+    },
+    destroy_hub: function(request)
+    {
+
+    }
+}
