72b08e71f28b208ee1f47cc2814213d546a50642
Jason Hanf <jasonhanf@fastcomm.com>
2021-03-23 12:35:15 +0200

    Created prepaid-balance-controller

---
+++ app/controllers/prepay_balance_controller.js
@@ -0,0 +1,151 @@
+//  === Requirements ===
+const faker = require('faker')
+
+//  === Controller ===
+module.exports = {
+    index_product_buckets: function(request) 
+    {
+        console.log("prepay_balance_controller.index_product_buckets")
+    },
+    show_product_buckets: function(request)
+    {
+        console.log("prepay_balance_controller.show_product_buckets")
+    },
+    create_product_adjustments: function(request)
+    {
+        console.log("prepay_balance_controller.create_product_adjustments")
+    },
+    index_product_adjustments: function(request)
+    {
+        console.log("prepay_balance_controller.index_product_adjustments")
+    },
+    show_product_adjustments: function(request)
+    {
+        console.log("prepay_balance_controller.show_product_adjustments")
+    },
+    create_product_transfers: function(request)
+    {
+        console.log("prepay_balance_controller.create_product_transfers")
+    },
+    index_product_transfers: function(request)
+    {
+        console.log("prepay_balance_controller.index_product_transfers")
+    },
+    create_product_topups: function(request)
+    {
+        console.log("prepay_balance_controller.create_product_topups")
+    },
+    index_product_topups: function(request)
+    {
+        console.log("prepay_balance_controller.index_product_topups")
+    },
+    create_product_topups_status: function(request)
+    {
+        console.log("prepay_balance_controller.create_product_topups_status")
+    },
+    index_product_topups_status: function(request)
+    {
+        console.log("prepay_balance_controller.index_product_topups_status")
+    },
+    index_buckets: function(request)
+    {
+        console.log("prepay_balance_controller.index_buckets")
+    },
+    show_buckets: function(request)
+    {
+        console.log("prepay_balance_controller.show_buckets")
+    },
+    index_accumulated_balance: function(request)
+    {
+        console.log("prepay_balance_controller.index_accumulated_balance")
+    },
+    show_accumulated_balance_products: function(request)
+    {
+        console.log("prepay_balance_controller.show_accumulated_balance_products")
+    },
+    index_activity: function(request)
+    {
+        console.log("prepay_balance_controller.index_activity")
+    },
+    show_activity_products: function(request)
+    {
+        console.log("prepay_balance_controller.show_activity_products")
+    },
+    create_topups: function(request)
+    {
+        console.log("prepay_balance_controller.create_topups")
+    },
+    index_topups: function(request)
+    {
+        console.log("prepay_balance_controller.index_topups")
+    },
+    show_topups: function(request)
+    {
+        console.log("prepay_balance_controller.show_topups")
+    },
+    index_topups_status: function(request)
+    {
+        console.log("prepay_balance_controller.index_topups_status")
+    },
+    update_topups_status: function(request)
+    {
+        console.log("prepay_balance_controller.update_topups_status")
+    },
+    create_transfers: function(request)
+    {
+        console.log("prepay_balance_controller.create_transfers")
+    },
+    index_transfers: function(request)
+    {
+        console.log("prepay_balance_controller.index_transfers")
+    },
+    show_transfers: function(request)
+    {
+        console.log("prepay_balance_controller.show_transfers")
+    },
+    index_transfers_status: function(request)
+    {
+        console.log("prepay_balance_controller.index_transfers_status")
+    },
+    update_transfers_status: function(request)
+    {
+        console.log("prepay_balance_controller.update_transfers_status")
+    },
+    create_adjustments: function(request)
+    {
+        console.log("prepay_balance_controller.create_adjustments")
+    },
+    index_adjustments: function(request)
+    {
+        console.log("prepay_balance_controller.index_adjustments")
+    },
+    show_adjustments: function(request)
+    {
+        console.log("prepay_balance_controller.show_adjustments")
+    },
+    index_hub: function(request)
+    {
+        console.log("prepay_balance_controller.index_hub")
+    },
+    show_hub: function(request)
+    {
+        console.log("prepay_balance_controller.show_hub")
+    },
+    create_client_listener: function(request)
+    {
+        console.log("prepay_balance_controller.create_client_listener")
+    },
+    reserve: function(request)
+    {
+        console.log("prepay_balance_controller.reserve")
+    },
+    unreserve: function(request)
+    {
+        console.log("prepay_balance_controller.unreserve")
+    },
+    deduct: function(request)
+    {
+        console.log("prepay_balance_controller.deduct")
+    }
+
+}
