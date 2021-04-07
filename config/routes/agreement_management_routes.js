d64671c63d574cd27914b949a96ac9994b00edb0
Jason Hanf <jasonhanf@fastcomm.com>
2021-04-07 10:48:08 +0200

    updates made

--- config/routes/agreement_management_routes.js
+++ config/routes/agreement_management_routes.js
@@ -4,21 +4,10 @@ var agreement_management_routes = express.Router()
 var agreement_management_controller = require("../../app/controllers/agreement_management_controller")
 
 //  === Routes ===
-agreement_management_routes.get('/appointment', (request, response) => {
-  response.send( account_management_controller.index_party(request) )
+agreement_management_routes.get('/', (request, response) => {
+  response.send( agreement_management_controller.index(request) )
 })
-agreement_management_routes.post('/appointment', (request, response) => {
-  response.send( account_management_controller.create_party(request) )
-})
-agreement_management_routes.get('/appointment/:id', (request, response) => {
-  response.send( account_management_controller.show_party(request) )
-})
-agreement_management_routes.patch('/appointment/:id', (request, response) => {
-  response.send( account_management_controller.update_party(request) )
-})
-agreement_management_routes.delete('/appointment/:id', (request, response) => {
-  response.send( account_management_controller.destroy_party(request) )
-})
+
 
 //  === Exports ===
 module.exports = agreement_management_routes
