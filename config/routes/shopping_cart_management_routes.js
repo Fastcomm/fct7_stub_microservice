6312e2342fc455e5260ebf8f3dae9ca014c55b05
Jason Hanf <jasonhanf@fastcomm.com>
2021-04-12 15:53:18 +0200

    added shopping cart stub

---
+++ config/routes/shopping_cart_management_routes.js
@@ -0,0 +1,30 @@
+//  === Requirements ===
+var express = require('express')
+var shopping_cart_management_routes = express.Router()
+var shopping_cart_management_controller = require("../../app/controllers/shopping_cart_management_controller")
+
+//  === Routes ===
+shopping_cart_management_routes.get('/', (request, response) => {
+  response.send( shopping_cart_management_controller.index(request) )
+})
+shopping_cart_management_routes.post('/', (request, response) => {
+    response.send( shopping_cart_management_controller.create(request) )
+})
+shopping_cart_management_routes.get('/:id', (request, response) => {
+    response.send( shopping_cart_management_controller.show(request) )
+})
+shopping_cart_management_routes.patch('/:id', (request, response) => {
+    response.send( shopping_cart_management_controller.update(request) )
+})
+shopping_cart_management_routes.delete('/:id', (request, response) => {
+    response.send( shopping_cart_management_controller.destroy(request) )
+})
+shopping_cart_management_routes.post('/hub', (request, response) => {
+    response.send( shopping_cart_management_controller.create_hub(request) )
+})
+shopping_cart_management_routes.delete('/hub/:id', (request, response) => {
+    response.send( shopping_cart_management_controller.destroy_hub(request) )
+})
+
+//  === Exports ===
+module.exports = shopping_cart_management_routes
