7418088688a03f0187d24a09a4ffedf14ccf6caa
VF-ROOT\hanfj001 <jasondaniel.hanf@vcontractor.co.za>
2021-04-20 15:35:02 +0200

    saving work

--- config/routes/payment_management_routes.js
+++ config/routes/payment_management_routes.js
@@ -4,8 +4,25 @@ var payment_management_routes = express.Router()
 var payment_management_controller = require("../../app/controllers/payment_management_controller")
 
 //  === Routes ===
+
+payment_management_routes.get('/refund', (request, response) => {
+    response.send( payment_management_controller.index(request) )
+})
+payment_management_routes.post('/refund', (request, response) => {
+    response.send( payment_management_controller.index(request) )
+})
+payment_management_routes.get('/refund/:refund_id', (request, response) => {
+    response.send( payment_management_controller.index(request) )
+})
+
 payment_management_routes.get('/', (request, response) => {
-  response.send( payment_management_controller.index(request) )
+    response.send( payment_management_controller.index(request) )
+})
+payment_management_routes.post('/', (request, response) => {
+    response.send( payment_management_controller.index(request) )
+})
+payment_management_routes.get('/:payment_id', (request, response) => {
+    response.send( payment_management_controller.index(request) )
 })
 
 
