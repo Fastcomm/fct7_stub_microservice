345321e9bd8cdda5489258a8482fd5ea95891574
Riaan-Dowling <riaan.dowling@hotmail.com>
2021-05-19 14:01:35 +0200

    product offering

--- config/routes/product_offering_qualification_routes.js
+++ config/routes/product_offering_qualification_routes.js
@@ -7,6 +7,18 @@ var product_offering_qualification_controller = require("../../app/controllers/p
 product_offering_qualification_routes.get('/', (request, response) => {
   response.send( product_offering_qualification_controller.index(request) )
 })
+product_offering_qualification_routes.post('/', (request, response) => {
+  response.send( product_offering_qualification_controller.create(request) )
+})
+product_offering_qualification_routes.get('/:id', (request, response) => {
+  response.send( product_offering_qualification_controller.show(request) )
+})
+product_offering_qualification_routes.patch('/:id', (request, response) => {
+  response.send( product_offering_qualification_controller.update(request) )
+})
+product_offering_qualification_routes.delete('/:id', (request, response) => {
+  response.send( product_offering_qualification_controller.destroy(request) )
+})
 
 //  === Exports ===
 module.exports = product_offering_qualification_routes
